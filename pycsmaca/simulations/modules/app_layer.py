563d330e98d9e11bc89f21e97d336f62fab59089
Andrey Larionov <larioandr@gmail.com>
2019-06-18 17:11:15 +0300

    Add support for finite number of packets generation when intervals or data_size are given with tuples in RandomSource

--- pycsmaca/simulations/modules/app_layer.py
+++ pycsmaca/simulations/modules/app_layer.py
@@ -55,23 +55,36 @@ class RandomSource(Model):
         """Create `RandomSource` module.
 
         :param sim: `pydesim.Simulator` object;
-        :param data_size: callable without arguments or constant, application
-            data size distribution;
-        :param interval: callable without arguments or constant, inter-arrival
-            intervals distribution;
+        :param data_size: callable without arguments, iterable or constant;
+            represents application data size distribution;
+        :param interval: callable without arguments, iterable or constant;
+            represents inter-arrival intervals distribution;
         :param source_id: this source ID (more like IP address, not MAC)
         :param dest_addr: destination MAC address.
         """
         super().__init__(sim)
-        self.data_size = data_size
-        self.interval = interval
-        self.source_id = source_id
-        self.dest_addr = dest_addr
+        self.__data_size = data_size
+        self.__interval = interval
+        self.__source_id = source_id
+        self.__dest_addr = dest_addr
+
+        # Attempt to build iterators for data size and intervals:
+        try:
+            self.__interval_iter = iter(self.__interval)
+        except TypeError:
+            self.__interval_iter = None
+
+        try:
+            self.__data_size_iter = iter(self.__data_size)
+        except TypeError:
+            self.__data_size_iter = None
+
         # Statistics:
         self.__arrival_intervals = Intervals()
         self.__data_size_stat = Statistic()
+
         # Initialize:
-        sim.schedule(self.__get_next_interval(), self._generate)
+        self.__schedule_next_arrival()
 
     @property
     def arrival_intervals(self):
@@ -81,27 +94,62 @@ class RandomSource(Model):
     def data_size_stat(self):
         return self.__data_size_stat
 
+    @property
+    def data_size(self):
+        return self.__data_size
+
+    @property
+    def interval(self):
+        return self.__interval
+
+    @property
+    def source_id(self):
+        return self.__source_id
+
+    @property
+    def dest_addr(self):
+        return self.__dest_addr
+
     def _generate(self):
-        data_size = self.__get_next_size()
-        app_data = AppData(dest_addr=self.dest_addr, size=data_size,
-                           source_id=self.source_id)
-        self.connections['network'].send(app_data)
-        self.sim.schedule(self.__get_next_interval(), self._generate)
-        # Recording statistics:
-        self.arrival_intervals.record(self.sim.stime)
-        self.data_size_stat.append(data_size)
+        try:
+            data_size = self.__get_next_size()
+        except StopIteration:
+            pass  # do nothing if stop iteration fired
+        else:
+            app_data = AppData(dest_addr=self.dest_addr, size=data_size,
+                               source_id=self.source_id)
+            self.connections['network'].send(app_data)
+            self.__schedule_next_arrival()
+            # Recording statistics:
+            self.arrival_intervals.record(self.sim.stime)
+            self.data_size_stat.append(data_size)
 
     def __get_next_interval(self):
+        if self.__interval_iter is not None:
+            return next(self.__interval_iter)
         try:
             return self.interval()
         except TypeError:
             return self.interval
 
     def __get_next_size(self):
+        if self.__data_size_iter:
+            return next(self.__data_size_iter)
         try:
             return self.data_size()
         except TypeError:
             return self.data_size
 
+    def __schedule_next_arrival(self):
+        try:
+            self.sim.schedule(self.__get_next_interval(), self._generate)
+        except StopIteration:
+            pass
+
     def __str__(self):
         return f'Source({self.source_id})'
+
+
+class Sink(Model):
+    def __init__(self, sim):
+        super().__init__(sim)
--- tests/unit_tests/test_app_layer_modules.py
+++ tests/unit_tests/test_app_layer_modules.py
@@ -1,8 +1,7 @@
-import numpy as np
 import pytest
 from pydesim import Model, simulate
-from unittest.mock import Mock, patch, MagicMock
-from pycsmaca.simulations.modules.app_layer import RandomSource, AppData
+from unittest.mock import Mock, patch, ANY
+from pycsmaca.simulations.modules.app_layer import RandomSource, AppData, Sink
 
 
 class DummyModel(Model):
@@ -15,6 +14,10 @@ class DummyModel(Model):
     def __str__(self):
         return self.name
 
+
+#############################################################################
+# TEST RandomSource MODULE
+#############################################################################
 
 # noinspection PyProtectedMember
 def test_random_source_generates_packets():
@@ -85,6 +88,60 @@ def test_random_source_can_use_constant_distributions():
 
 
 # noinspection PyProtectedMember
+def test_random_source_can_use_finite_intervals_distributions():
+    """Validate that `RandomSource` will stop when intervals is finite tuple.
+    """
+    sim = Mock()
+    sim.stime = 0
+    source = RandomSource(
+        sim, data_size=123, interval=(34, 42,), source_id=0, dest_addr=1)
+
+    network_service_mock = Mock()
+    source.connections['network'] = network_service_mock
+    rev_conn = source.connections['network'].reverse
+
+    sim.schedule.assert_called_with(34, source._generate)
+    sim.schedule.reset_mock()
+
+    source._generate()
+    sim.schedule.assert_any_call(42, source._generate)
+    sim.schedule.reset_mock()
+
+    source._generate()
+    sim.schedule.assert_called_once_with(
+        0, network_service_mock.handle_message, args=(ANY,),
+        kwargs={'sender': source, 'connection': rev_conn}
+    )
+
+
+# noinspection PyProtectedMember
+def test_random_source_can_use_finite_data_size_distributions():
+    """Validate that `RandomSource` will stop when data size is finite tuple.
+    """
+    sim = Mock()
+    sim.stime = 0
+    source = RandomSource(
+        sim, data_size=(10, 20), interval=100, source_id=0, dest_addr=1)
+
+    network_service_mock = Mock()
+    source.connections['network'] = network_service_mock
+
+    with patch('pycsmaca.simulations.modules.app_layer.AppData') as AppDataMock:
+        source._generate()
+        AppDataMock.assert_called_with(dest_addr=1, source_id=0, size=10)
+        AppDataMock.reset_mock()
+
+        source._generate()
+        AppDataMock.assert_called_with(dest_addr=1, source_id=0, size=20)
+        AppDataMock.reset_mock()
+
+        sim.schedule.reset_mock()
+        source._generate()
+        AppDataMock.assert_not_called()
+        sim.schedule.assert_not_called()
+
+
+# noinspection PyProtectedMember
 def test_random_source_provides_intervals_and_sizes_statistics():
     """Validate that `RandomSource` provides statistics.
     """
@@ -109,11 +166,16 @@ def test_random_source_provides_intervals_and_sizes_statistics():
 
     # Also check that we can not replace statistics:
     with pytest.raises(AttributeError):
-        ret.data.source.arrival_intervals = 'another value'
+        from pydesim import Intervals
+        ret.data.source.arrival_intervals = Intervals()
     with pytest.raises(AttributeError):
-        ret.data.source.data_size_stat = 'another value'
+        from pydesim import Statistic
+        ret.data.source.data_size_stat = Statistic()
 
 
+#############################################################################
+# TEST AppData PACKETS
+#############################################################################
 # noinspection PyPropertyAccess
 def test_app_data_is_immutable():
     app_data = AppData(dest_addr=5, size=20, source_id=13)
@@ -131,3 +193,20 @@ def test_app_data_is_immutable():
 def test_app_data_provides_str():
     app_data = AppData(dest_addr=1, size=250, source_id=2)
     assert str(app_data) == 'AppData{sid=2,dst=1,size=250}'
+
+
+#############################################################################
+# TEST Sink MODULE
+#############################################################################
+# def test_sink_module_accept_app_data():
+#     class TestModel(Model):
+#         def __init__(self, sim):
+#             super().__init__(sim)
+#             self.sink = Sink()
+#             self.sources = [
+#                 RandomSource(sim, 100, Mock(side_effect=(10, 100, 200)), 0, 5),
+#                 RandomSource(sim, 300, Mock(side_effect=(20, 400, 80)), 1, 5),
+#             ]
+#             for source in self.sources:
+#                 conn = source.connections.set('sink', self.sink, reverse=False)
+#                 conn.delay = Mock()
