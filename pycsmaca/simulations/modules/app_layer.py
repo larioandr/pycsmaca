b640991495ebf69458e0944aed0162c57417fe0e
Andrey Larionov <larioandr@gmail.com>
2019-06-18 16:13:26 +0300

    Add statistics and __str__ method to RandomSource, add documentation to RandomSource

--- pycsmaca/simulations/modules/app_layer.py
+++ pycsmaca/simulations/modules/app_layer.py
@@ -1,4 +1,4 @@
-from pydesim import Model
+from pydesim import Model, Intervals, Statistic
 
 
 class AppData:
@@ -28,20 +28,80 @@ class AppData:
 
 
 class RandomSource(Model):
+    """This module provides data source with independent intervals and sizes.
+
+    `RandomSource` generates `AppData` packets with a given bit size
+    distribution. Inter-arrival intervals are also randomly distributed,
+    and this distribution is independent from data size distribution.
+
+    Note: distributions are passed to the constructor as callable objects,
+    but they also can be specified with constants.
+
+    Source directs its packets to network layer. Packets have a given
+    destination address. Source is specified with its SourceID.
+
+    Provided statistics:
+    - `arrival_intervals`: inter-arrival intervals;
+    - `data_size_stat`: generated data sizes statistics.
+
+    Events:
+    - timeout: fired when inter-arrival interval is reached.
+
+    Connections:
+    - 'network': connected network layer module; should implement
+        `handle_message(app_data)` method.
+    """
     def __init__(self, sim, data_size, interval, source_id, dest_addr):
+        """Create `RandomSource` module.
+
+        :param sim: `pydesim.Simulator` object;
+        :param data_size: callable without arguments or constant, application
+            data size distribution;
+        :param interval: callable without arguments or constant, inter-arrival
+            intervals distribution;
+        :param source_id: this source ID (more like IP address, not MAC)
+        :param dest_addr: destination MAC address.
+        """
         super().__init__(sim)
         self.data_size = data_size
         self.interval = interval
         self.source_id = source_id
         self.dest_addr = dest_addr
+        # Statistics:
+        self.__arrival_intervals = Intervals()
+        self.__data_size_stat = Statistic()
         # Initialize:
-        sim.schedule(interval(), self._generate)
+        sim.schedule(self.__get_next_interval(), self._generate)
+
+    @property
+    def arrival_intervals(self):
+        return self.__arrival_intervals
+
+    @property
+    def data_size_stat(self):
+        return self.__data_size_stat
 
     def _generate(self):
-        app_data = AppData(dest_addr=self.dest_addr, size=self.data_size(),
+        data_size = self.__get_next_size()
+        app_data = AppData(dest_addr=self.dest_addr, size=data_size,
                            source_id=self.source_id)
         self.connections['network'].send(app_data)
-        self.sim.schedule(self.interval(), self._generate)
+        self.sim.schedule(self.__get_next_interval(), self._generate)
+        # Recording statistics:
+        self.arrival_intervals.record(self.sim.stime)
+        self.data_size_stat.append(data_size)
 
-    def handle_message(self, message, sender=None):
-        pass
+    def __get_next_interval(self):
+        try:
+            return self.interval()
+        except TypeError:
+            return self.interval
+
+    def __get_next_size(self):
+        try:
+            return self.data_size()
+        except TypeError:
+            return self.data_size
+
+    def __str__(self):
+        return f'Source({self.source_id})'
--- tests/unit_tests/test_app_layer_modules.py
+++ tests/unit_tests/test_app_layer_modules.py
@@ -1,29 +1,45 @@
+import numpy as np
 import pytest
-from pydesim import Model
-from unittest.mock import Mock, patch
+from pydesim import Model, simulate
+from unittest.mock import Mock, patch, MagicMock
 from pycsmaca.simulations.modules.app_layer import RandomSource, AppData
+
+
+class DummyModel(Model):
+    """We use this `DummyModel` when we need a full-functioning model.
+    """
+    def __init__(self, sim, name):
+        super().__init__(sim)
+        self.name = name
+
+    def __str__(self):
+        return self.name
 
 
 # noinspection PyProtectedMember
 def test_random_source_generates_packets():
+    """In this test we check that `RandomSource` properly generates `AppData`.
+    """
+    # First, we create the `RandomSource` module, validate it is
+    # inherited from `pydesim.Module` and check that upon construction source
+    # scheduled the next packet arrival as specified by `interval` parameter:
     sim = Mock()
+    sim.stime = 0
     source = RandomSource(sim, data_size=Mock(return_value=42),
                           interval=Mock(side_effect=(74, 21)), source_id=34,
                           dest_addr=13)
     assert isinstance(source, Model)
+    sim.schedule.assert_called_with(74, source._generate)
 
-    # Provide necessary connections:
+    # Define a mock for NetworkLayer module and establish a connection:
     network_service_mock = Mock()
     source.connections['network'] = network_service_mock
 
-    # First, check that upon construction source scheduled the next packet
-    # arrival as specified by `interval` parameter:
-    sim.schedule.assert_called_with(74, source._generate)
-
-    # Then, we call that method and make sure that it sends a packet via the
-    # 'network' connection. Exactly it means that the connected module
-    # `handle_message(packet)` method is called using `sim.schedule`, which
-    # is expected to be called from within `source.connections`
+    # Now we call method `_generate()` method and make sure that it sends a
+    # packet via the 'network' connection.
+    # Exactly it means that the connected module `handle_message(packet)`
+    # method is called using `sim.schedule`, which is expected to be called
+    # from within `source.connections['network']` connection.
     with patch('pycsmaca.simulations.modules.app_layer.AppData') as AppDataMock:
         _spec = dict(dest_addr=13, size=42, source_id=34)
         _packet = Mock(**_spec)
@@ -32,13 +48,70 @@ def test_random_source_generates_packets():
         source._generate()
 
         AppDataMock.assert_called_with(**_spec)
+
+        rev_conn = source.connections['network'].reverse
         sim.schedule.assert_any_call(
             0, network_service_mock.handle_message, args=(_packet,),
-            kwargs={'sender': source}
+            kwargs={'sender': source, 'connection': rev_conn}
         )
 
-    # Make sure that after the _generate() call another event was scheduled:
+    # Finally, we make sure that after the _generate() call another event
+    # was scheduled:
     sim.schedule.assert_any_call(21, source._generate)
+
+
+# noinspection PyProtectedMember
+def test_random_source_can_use_constant_distributions():
+    """Validate that numeric constants can be used instead of distributions.
+    """
+    sim = Mock()
+    sim.stime = 0
+    source = RandomSource(
+        sim, data_size=123, interval=34, source_id=0, dest_addr=1)
+
+    network_service_mock = Mock()
+    source.connections['network'] = network_service_mock
+    sim.schedule.assert_called_with(34, source._generate)
+
+    with patch('pycsmaca.simulations.modules.app_layer.AppData') as AppDataMock:
+        _spec = dict(dest_addr=1, size=123, source_id=0)
+        _packet = Mock(**_spec)
+        AppDataMock.return_value = _packet
+
+        source._generate()
+        AppDataMock.assert_called_with(**_spec)
+
+    sim.schedule.assert_any_call(34, source._generate)
+
+
+# noinspection PyProtectedMember
+def test_random_source_provides_intervals_and_sizes_statistics():
+    """Validate that `RandomSource` provides statistics.
+    """
+    intervals = (10, 12, 15, 17)
+    data_size = (123, 453, 245, 321)
+
+    class TestModel(Model):
+        def __init__(self, sim):
+            super().__init__(sim)
+            self.source = RandomSource(
+                sim, source_id=34, dest_addr=13,
+                data_size=Mock(side_effect=data_size),
+                interval=Mock(side_effect=(intervals + (1000,))),
+            )
+            self.network = DummyModel(sim, 'Network')
+            self.source.connections['network'] = self.network
+
+    ret = simulate(TestModel, stime_limit=sum(intervals))
+
+    assert ret.data.source.arrival_intervals.as_tuple() == intervals
+    assert ret.data.source.data_size_stat.as_tuple() == data_size
+
+    # Also check that we can not replace statistics:
+    with pytest.raises(AttributeError):
+        ret.data.source.arrival_intervals = 'another value'
+    with pytest.raises(AttributeError):
+        ret.data.source.data_size_stat = 'another value'
 
 
 # noinspection PyPropertyAccess
