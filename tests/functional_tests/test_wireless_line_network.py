7594717307a5186979d5117cfdd2493e9c188a40
Andrey Larionov <larioandr@gmail.com>
2019-06-21 21:35:32 +0300

    Add functional test for wireless line network

---
+++ tests/functional_tests/test_wireless_line_network.py
@@ -0,0 +1,230 @@
+from math import floor
+
+import pytest
+from numpy.testing import assert_allclose
+from pydesim import simulate, Logger
+from pyqumo.distributions import Constant, Exponential
+
+from pycsmaca.simulations import WirelessLineNetwork
+
+
+SIM_TIME_LIMIT = 1000
+PAYLOAD_SIZE = Constant(100.0)      # 100 bits data payload
+SOURCE_INTERVAL = Constant(1.0)     # 1 second between packets
+MAC_HEADER = 50             # bits
+PHY_HEADER = 25             # bits
+PREAMBLE = 1e-3             # seconds
+BITRATE = 500               # 500 bps
+DIFS = 200e-3               # 200 ms
+SIFS = 100e-3               # 100 ms
+SLOT = 50e-3                # 50 ms
+CWMIN = 2
+CWMAX = 8
+ACK_SIZE = 100              # 100 bits ACK (without PHY header!)
+DISTANCE = 500              # 500 meters between stations
+CONNECTION_RADIUS = 750     # 750 meters (all stations in circle are connected)
+SPEED_OF_LIGHT = 10000      # 10 kilometers per second speed of light
+
+
+def test_two_dcf_stations():
+    sr = simulate(
+        WirelessLineNetwork,
+        stime_limit=SIM_TIME_LIMIT,
+        params=dict(
+            num_stations=2,
+            active_sources=[0],
+            payload_size=PAYLOAD_SIZE,
+            source_interval=SOURCE_INTERVAL,
+            mac_header_size=MAC_HEADER,
+            phy_header_size=PHY_HEADER,
+            ack_size=ACK_SIZE,
+            preamble=PREAMBLE,
+            bitrate=BITRATE,
+            difs=DIFS,
+            sifs=SIFS,
+            slot=SLOT,
+            cwmin=CWMIN,
+            cwmax=CWMAX,
+            distance=DISTANCE,
+            connection_radius=CONNECTION_RADIUS,
+            speed_of_light=SPEED_OF_LIGHT,
+        ),
+        loglevel=Logger.Level.INFO
+    )
+
+    client = sr.data.stations[0]
+    server = sr.data.stations[1]
+
+    expected_number_of_packets = floor(SIM_TIME_LIMIT / SOURCE_INTERVAL.mean())
+
+    assert client.source.num_packets_sent == expected_number_of_packets
+    assert (expected_number_of_packets - 1 <=
+            server.sink.num_packets_received <= expected_number_of_packets)
+
+    mean_payload = PAYLOAD_SIZE.mean()
+    expected_service_time = (
+            DIFS + CWMIN/2 * SLOT
+            + PREAMBLE + (mean_payload + MAC_HEADER + PHY_HEADER) / BITRATE
+            + SIFS + PREAMBLE + (PHY_HEADER + ACK_SIZE) / BITRATE
+            + 2 * DISTANCE / SPEED_OF_LIGHT
+
+    )
+
+    source_id = client.source.source_id
+    assert_allclose(
+        server.sink.source_delays[source_id].mean(),
+        expected_service_time,
+        rtol=0.1
+    )
+
+    client_if = client.get_interface_to(server)
+    assert client_if.queue.size_trace.timeavg() == 0
+
+    assert_allclose(
+        client_if.mac.transmitter.busy_trace.timeavg(),
+        expected_service_time / SOURCE_INTERVAL.mean(),
+        rtol=0.1
+    )
+
+
+@pytest.mark.parametrize('num_stations', [3, 4])
+def test_dcf_line_network_with_single_source(num_stations):
+    sr = simulate(
+        WirelessLineNetwork,
+        stime_limit=SIM_TIME_LIMIT,
+        params=dict(
+            num_stations=num_stations,
+            active_sources=[0],
+            payload_size=PAYLOAD_SIZE,
+            source_interval=SOURCE_INTERVAL,
+            mac_header_size=MAC_HEADER,
+            phy_header_size=PHY_HEADER,
+            ack_size=ACK_SIZE,
+            preamble=PREAMBLE,
+            bitrate=BITRATE,
+            difs=DIFS,
+            sifs=SIFS,
+            slot=SLOT,
+            cwmin=CWMIN,
+            cwmax=CWMAX,
+            distance=DISTANCE,
+            connection_radius=CONNECTION_RADIUS,
+            speed_of_light=SPEED_OF_LIGHT,
+        ),
+        loglevel=Logger.Level.ERROR
+    )
+
+    client = sr.data.stations[0]
+    server = sr.data.stations[-1]
+    source_id = client.source.source_id
+
+    expected_number_of_packets = floor(SIM_TIME_LIMIT / SOURCE_INTERVAL.mean())
+
+    assert client.source.num_packets_sent == expected_number_of_packets
+    assert (expected_number_of_packets - 1 <=
+            server.sink.num_packets_received <= expected_number_of_packets)
+
+    mean_payload = PAYLOAD_SIZE.mean()
+    expected_service_time = (
+            DIFS + CWMIN/2 * SLOT
+            + PREAMBLE + (mean_payload + MAC_HEADER + PHY_HEADER) / BITRATE
+            + SIFS + PREAMBLE + (PHY_HEADER + ACK_SIZE) / BITRATE
+            + 2 * DISTANCE / SPEED_OF_LIGHT
+
+    )
+    expected_end_to_end_delay = expected_service_time * (num_stations - 1)
+
+    assert_allclose(
+        server.sink.source_delays[source_id].mean(),
+        expected_end_to_end_delay,
+        rtol=0.1
+    )
+
+    client_if = client.get_interface_to(server)
+    assert client_if.queue.size_trace.timeavg() == 0
+
+    assert_allclose(
+        client_if.mac.transmitter.busy_trace.timeavg(),
+        expected_service_time / SOURCE_INTERVAL.mean(),
+        rtol=0.1
+    )
+
+
+@pytest.mark.parametrize('num_stations', [3, 4])
+def test_wired_line_network_with_cross_traffic(num_stations):
+    sr = simulate(
+        WirelessLineNetwork,
+        stime_limit=SIM_TIME_LIMIT,
+        params=dict(
+            num_stations=num_stations,
+            active_sources=range(num_stations - 1),
+            payload_size=PAYLOAD_SIZE,
+            source_interval=SOURCE_INTERVAL,
+            mac_header_size=MAC_HEADER,
+            phy_header_size=PHY_HEADER,
+            ack_size=ACK_SIZE,
+            preamble=PREAMBLE,
+            bitrate=BITRATE,
+            difs=DIFS,
+            sifs=SIFS,
+            slot=SLOT,
+            cwmin=CWMIN,
+            cwmax=CWMAX,
+            distance=DISTANCE,
+            connection_radius=CONNECTION_RADIUS,
+            speed_of_light=SPEED_OF_LIGHT,
+        ),
+        loglevel=Logger.Level.ERROR
+    )
+
+    client = sr.data.stations[0]
+    server = sr.data.stations[-1]
+    source_id = client.source.source_id
+
+    expected_number_of_packets = floor(SIM_TIME_LIMIT / SOURCE_INTERVAL.mean())
+
+    assert_allclose(
+        client.source.num_packets_sent,
+        expected_number_of_packets,
+        rtol=0.1
+    )
+    assert_allclose(
+        server.sink.num_packets_received,
+        (num_stations - 1) * expected_number_of_packets,
+        rtol=0.1
+    )
+
+    mean_payload = PAYLOAD_SIZE.mean()
+    expected_service_time = (
+            DIFS + CWMIN/2 * SLOT
+            + PREAMBLE + (mean_payload + MAC_HEADER + PHY_HEADER) / BITRATE
+            + SIFS + PREAMBLE + (PHY_HEADER + ACK_SIZE) / BITRATE
+            + 2 * DISTANCE / SPEED_OF_LIGHT
+
+    )
+    delay_low_bound = expected_service_time * (num_stations - 1) * 0.9999
+    assert server.sink.source_delays[source_id].mean() >= delay_low_bound
+
+    expected_busy_ratio = expected_service_time / SOURCE_INTERVAL.mean()
+    client_iface = sr.data.get_tx_iface(0)
+    assert_allclose(
+        client_iface.mac.transmitter.busy_trace.timeavg(),
+        expected_busy_ratio,
+        rtol=0.1
+    )
+
+    # Here we make sure that out interfaces for all middle stations
+    # have non-empty queues since they also generate traffic at almost the same
+    # time as they receive packets from connected stations:
+    for i in range(1, num_stations - 1):
+        sta = sr.data.stations[i]
+        next_sta = sr.data.stations[i + 1]
+        sta_if = sta.get_interface_to(next_sta)
+        assert sta_if.queue.size_trace.timeavg() > 0
+        if i > 0:
+            sta_inp_if = sta.get_interface_to(sr.data.stations[i - 1])
+            tx_busy_rate = sta_if.transceiver.tx_busy_trace.timeavg()
+            rx_busy_rate = sta_inp_if.transceiver.rx_busy_trace.timeavg()
+            assert_allclose(
+                tx_busy_rate, rx_busy_rate + expected_busy_ratio, rtol=0.1
+            )
